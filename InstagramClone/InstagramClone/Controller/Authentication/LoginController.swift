f7372c30c7369b38617d22c2c8888c7a868438e4
Ali Eldeeb <aeldeeb@fordham.edu>
2022-11-11 20:02:07 -0800

    Feat: Login and sign up form validation and profile picker implemented

--- InstagramClone/InstagramClone/Controller/Authentication/LoginController.swift
+++ InstagramClone/InstagramClone/Controller/Authentication/LoginController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class LoginController: UIViewController{
     //MARK: - Properties
+    
+    //instance of our viewModel
+    private var viewModel = LoginViewModel()
+    
     private let logoImageView: UIImageView = {
         let iv = UIImageView()
         iv.setDimensions(height: 80, width: 120)
@@ -29,9 +33,11 @@ class LoginController: UIViewController{
         return tf
     }()
     
-    private let loginButton: UIButton = {
-        let button = UIButton().makeButton(withTitle: "Log In", titleColor: .white, buttonColor: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), isRounded: true)
+    private lazy var loginButton: UIButton = {
+        let button = UIButton().makeButton(withTitle: "Log In", titleColor: .white, buttonColor: #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), isRounded: true)
         button.setDimensions(height: 50)
+        button.isEnabled = false
+        button.addTarget(self, action: #selector(handleLoginButtonPressed), for: .touchUpInside)
         return button
     }()
     
@@ -51,6 +57,7 @@ class LoginController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        configureNotificationObservers()
     }
     
     //MARK: - Helpers
@@ -78,9 +85,40 @@ class LoginController: UIViewController{
         dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, paddingBottom: 12)
     }
     
+    private func configureNotificationObservers(){
+        emailTextfield.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        passwordTextfield.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+    }
+    
     //MARK: - Selectors
     @objc private func handleSignUpButtonTapped(){
         let controller = RegistrationController()
         navigationController?.pushViewController(controller, animated: true)
     }
+    
+    @objc private func textDidChange(sender: UITextField){
+        //we added a sender so we can differentiate between different textfields
+        if sender == emailTextfield{
+            viewModel.email = sender.text
+        }else{
+            viewModel.password = sender.text
+        }
+        updateForm()
+    }
+    
+    @objc private func handleLoginButtonPressed(){
+        print("Push Maintab controller, user is authenticated")
+    }
+}
+
+//MARK: - FormViewModel
+extension LoginController: FormViewModel{
+    func updateForm() {
+        //by using the viewModel we can extract the logic for updating the view from the controller. THe viewModel computed properties handle all of that for us.
+        loginButton.backgroundColor = viewModel.buttonBackgroundColor
+        loginButton.setTitleColor(viewModel.buttonTitleColor, for: .normal)
+        loginButton.isEnabled = viewModel.formIsValid //if formIsValid we will enable the button, else disable it
+    }
+    
+    
 }
--- InstagramClone/InstagramClone/Controller/Authentication/RegistrationController.swift
+++ InstagramClone/InstagramClone/Controller/Authentication/RegistrationController.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class RegistrationController: UIViewController{
+    
     //MARK: - Properties
-    private let addPhotoButton: UIButton = {
+    private var viewModel = RegistrationViewModel()
+    
+    private lazy var addPhotoButton: UIButton = {
         let button = UIButton().makeButton(withImage: UIImage(named: "plus_photo"), isRounded: false)
         button.tintColor = .white
+        button.addTarget(self, action: #selector(handleProfileSelect), for: .touchUpInside)
+        button.setDimensions(height: 150, width: 150)
         return button
+    }()
+    
+    private lazy var photoPicker: UIImagePickerController = {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        return picker
     }()
     
     private let emailTextfield: UITextField = {
@@ -30,6 +42,14 @@ class RegistrationController: UIViewController{
     private let fullNameTextfield = UITextField().makeTextField(placeholder: " Fullname", isSecureField: false)
     private let userNameTextfield = UITextField().makeTextField(placeholder: " Username", isSecureField: false)
     
+    private lazy var signUpButton: UIButton = {
+        let button = UIButton().makeButton(withTitle: "Sign Up", titleColor: .white, buttonColor: #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), isRounded: true)
+        button.setDimensions(height: 50)
+        button.isEnabled = false
+        button.addTarget(self, action: #selector(handleUserRegistration), for: .touchUpInside)
+        return button
+    }()
+    
     private lazy var alreadyHaveAccountButton: UIButton = {
         let button = UIButton()
         button.attributedTitle(firstPart: "Already have an account? ", secondPart: "Log In")
@@ -42,6 +62,7 @@ class RegistrationController: UIViewController{
         super.viewDidLoad()
         configureGradientLayer()
         configureUI()
+        configureNotificationObservers()
     }
     
     //MARK: - Helpers
@@ -53,7 +74,7 @@ class RegistrationController: UIViewController{
         addPhotoButton.centerX(inView: view)
         addPhotoButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
         
-        let textFieldStack = UIStackView(arrangedSubviews: [emailTextfield, passwordTextfield, fullNameTextfield, userNameTextfield])
+        let textFieldStack = UIStackView(arrangedSubviews: [emailTextfield, passwordTextfield, fullNameTextfield, userNameTextfield, signUpButton])
         textFieldStack.axis = .vertical
         textFieldStack.spacing = 20
         textFieldStack.distribution = .fillEqually
@@ -66,9 +87,63 @@ class RegistrationController: UIViewController{
         
     }
     
+    private func configureNotificationObservers(){
+        emailTextfield.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        passwordTextfield.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        fullNameTextfield.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        userNameTextfield.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+    }
+    
     //MARK: - Selectors
+    
+    @objc private func handleProfileSelect(){
+        present(photoPicker, animated: true)
+    }
+    
+    @objc private func handleUserRegistration(){
+        print("User has signed up")
+    }
     
     @objc private func handleAlreadyHaveAccountTapped(){
         navigationController?.popViewController(animated: true)
+    }
+    
+    @objc private func textDidChange(sender: UITextField){
+        //we added a sender so we can differentiate between different textfields
+        switch sender{
+        case emailTextfield: viewModel.email = sender.text
+        case passwordTextfield: viewModel.password = sender.text
+        case fullNameTextfield: viewModel.fullname = sender.text
+        case userNameTextfield: viewModel.username = sender.text
+        default:
+            assertionFailure()
+        }
+        updateForm()
+        
+    }
+}
+
+//MARK: - FormViewModel
+extension RegistrationController: FormViewModel{
+    func updateForm() {
+        signUpButton.backgroundColor = viewModel.buttonBackgroundColor
+        signUpButton.setTitleColor(viewModel.buttonTitleColor, for: .normal)
+        signUpButton.isEnabled = viewModel.formIsValid //if formIsValid we will enable the button, else disable it
+    }
+}
+
+//MARK: - UIImagePickerControllerDelegate, UINavigationControllerDelegate
+extension RegistrationController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    //called once user has finished picking media type
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //info is a dictionary and we are looking for the edited image key's value and casting it as a UIImage
+        guard let selectedImage = info[.editedImage] as? UIImage else{ return }
+        addPhotoButton.layer.cornerRadius = addPhotoButton.frame.width / 2
+        addPhotoButton.layer.borderColor = UIColor.white.cgColor
+        addPhotoButton.layer.borderWidth = 2
+        addPhotoButton.setImage(selectedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        addPhotoButton.layer.masksToBounds = true
+        //dismiss the image picker
+        self.dismiss(animated: true)
     }
 }
